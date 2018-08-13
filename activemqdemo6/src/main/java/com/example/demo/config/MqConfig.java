38dfdf0bddabd76e5e39db84506df1a6c83bd209
wangmingzhe@travelsky.com <wangmingzhe@travlesky.com>
2018-08-13 13:27:09 +0800

    添加多个队列和多个发布订阅的配置方式

--- activemqdemo6/src/main/java/com/example/demo/config/MqConfig.java
+++ activemqdemo6/src/main/java/com/example/demo/config/MqConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo.config;
 
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
@@ -7,11 +10,45 @@ import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
 
 import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+import javax.jms.Topic;
 
 
 @EnableJms
 @Configuration
 public class MqConfig {
+
+    @Value("${application.topicDestinationName1}")
+    public String TOPIC1;
+
+    @Value("${application.topicDestinationName2}")
+    public String TOPIC2;
+
+    @Value("${application.queueDestinationName1}")
+    public String QUEUE1;
+
+    @Value("${application.queueDestinationName2}")
+    public String QUEUE2;
+
+    @Bean
+    public Topic topic1() {
+        return new ActiveMQTopic(TOPIC1);
+    }
+
+    @Bean
+    public Topic topic2() {
+        return new ActiveMQTopic(TOPIC2);
+    }
+
+    @Bean
+    public Queue queue1() {
+        return new ActiveMQQueue(QUEUE1);
+    }
+
+    @Bean
+    public Queue queue2() {
+        return new ActiveMQQueue(QUEUE2);
+    }
 
     @Bean
     public JmsListenerContainerFactory<?> topicListenerFactory(ConnectionFactory connectionFactory) {
